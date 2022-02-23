ad1c8490c9856f1c6dee2a2002f2fd38d91e090b
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-02-23 18:38:18 -0500

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database, then turn on server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
